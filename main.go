dd0e1059df2eae4a2c7029114b231c47b108aff2
Ciaran De Groot <ciaran.deg@gmail.com>
2024-02-17 11:16:43 -0700

    Load example.lock.hcl

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	sitter "github.com/smacker/go-tree-sitter"
 	"github.com/smacker/go-tree-sitter/hcl"
 )
 
 func main() {
+	filePath := "example.lock.hcl"
+	sourceCode, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
 	parser := sitter.NewParser()
 	parser.SetLanguage(hcl.GetLanguage())
 
-	sourceCode := []byte("provider \"foo\" \"bar\" {}")
 	tree, _ := parser.ParseCtx(context.Background(), nil, sourceCode)
 
 	n := tree.RootNode()
