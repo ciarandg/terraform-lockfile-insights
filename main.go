1fefd5e6b6fca405348cd11ed4c490ba6b92bfe7
Ciaran De Groot <ciaran.deg@gmail.com>
2024-02-17 11:09:07 -0700

    Add basic javascript parsing example

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/smacker/go-tree-sitter/javascript"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	parser := sitter.NewParser()
+	parser.SetLanguage(javascript.GetLanguage())
+
+	sourceCode := []byte("let a = 1")
+	tree, _ := parser.ParseCtx(context.Background(), nil, sourceCode)
+
+	n := tree.RootNode()
+
+	fmt.Println(n) // (program (lexical_declaration (variable_declarator (identifier) (number))))
+	
+	child := n.NamedChild(0)
+	fmt.Println(child.Type()) // lexical_declaration
+	fmt.Println(child.StartByte()) // 0
+	fmt.Println(child.EndByte()) // 9
 }
