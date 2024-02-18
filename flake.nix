66ef7f3d6521d4a286814fce4b212c8692932589
Ciaran De Groot <ciaran.deg@gmail.com>
2024-02-18 13:51:30 -0700

    Add aarch64-darwin to flake.nix systems

--- flake.nix
+++ flake.nix
@@ -11,7 +11,8 @@
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
-        "x86_64-linux"
+        "x86_64-linux",
+        "aarch64-darwin"
       ];
 
       perSystem = {
