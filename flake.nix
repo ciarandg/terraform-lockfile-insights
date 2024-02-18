4d28fe1e693758f6c690436747ac77516a32e9f9
Ciaran De Groot <ciaran.deg@gmail.com>
2024-02-18 13:28:55 -0700

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -4,14 +4,23 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, flake-parts } @ inputs: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "x86_64-linux"
-    ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+      ];
 
-    perSystem = { config, system, ... }: {
-      packages.terraform-lockfile-insights = nixpkgs.legacyPackages.${system}.hello;
-      packages.default = self.packages.${system}.terraform-lockfile-insights;
+      perSystem = {
+        config,
+        system,
+        ...
+      }: {
+        packages.terraform-lockfile-insights = nixpkgs.legacyPackages.${system}.hello;
+        packages.default = self.packages.${system}.terraform-lockfile-insights;
+      };
     };
-  };
 }
