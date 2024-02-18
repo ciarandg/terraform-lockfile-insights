e3ed86d97384f402f80941c71b41d64c28c2c574
Ciaran De Groot <ciaran.deg@gmail.com>
2024-02-18 13:21:39 -0700

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { self, nixpkgs, flake-parts } @ inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+    ];
+
+    perSystem = { config, system, ... }: {
+      packages.hello = nixpkgs.legacyPackages.${system}.hello;
+      packages.default = self.packages.${system}.hello;
+    };
+  };
+}
